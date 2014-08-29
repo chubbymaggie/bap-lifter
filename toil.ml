05265b81c2b4985cb38643b240e11cdace1f1476
Matthew Maurer <maurer@matthewmaurer.org>
2014-08-29 16:59:02 -0400

    Added format output specifier
    Change-Id: I05229a25f22c7042b7ac4eeef5c38093309d1db7

--- toil.ml
+++ toil.ml
@@ -6,6 +6,11 @@ let address = ref (Int64.of_int 0)
 let arch = ref None
 let dump_asm = ref false
 
+let text_format stmts =
+  String.concat ~sep:"\n" (List.map stmts ~f:Pp.string_of_bil)
+
+let formatter = ref text_format
+
 let arg_specs = [
   "--addr", Arg.String (fun x -> address := Int64.of_string x),
   "memory address of first instruction";
@@ -16,6 +21,13 @@ let arg_specs = [
       | _ -> raise (Arg.Bad "Invalid architecture: try x86, x86_64, arm")
     ), "one of x86, x86_64, arm";
   "--dump-asm", Arg.Set dump_asm, "show assembly (if available)";
+  "--format", Arg.String (function
+      | "json"  -> formatter := Bil_piqi.json_of_stmts
+      | "text"  -> formatter := text_format
+      | "proto" -> formatter := Bil_piqi.pb_of_stmts
+      | "xml"   -> formatter := Bil_piqi.xml_of_stmts
+      | _ -> raise (Arg.Bad "Invalid format. Available: json text proto xml")
+    ), "Data format. Available: json text proto xml";
 ]
 
 let () = Arg.parse arg_specs (fun _ -> ()) usage;
@@ -31,7 +43,7 @@ let () = Arg.parse arg_specs (fun _ -> ()) usage;
                                  !address)))
           (Bitvector.lit64 !address
              (Conceval.width_of LocalArch.mem_index_type)) in
-      List.iter bil ~f:(fun stmt -> print_endline (Pp.string_of_bil stmt));
+      print_string (!formatter bil);
       if !dump_asm then match asm with
         | Some asm -> print_endline asm
         | None -> print_endline "Assembly not available for this architecture."
